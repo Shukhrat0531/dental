7f40dc4000ef937dafd35580790abaae360df63c
Shukhrat0531 <shuhrat0531@gmail.com>
2025-12-26 14:47:14 +0500

    last4

--- app/main.py
+++ app/main.py
@@ -9,17 +9,17 @@ settings = get_settings()
 app = FastAPI(title=settings.app_name)
 
 # CORS (нужно для Flutter Web и для любых запросов из браузера)
-allowed_origins = [
-    "http://localhost:3000",
+allow_origins = [
+    "https://b1d6fa20-0f2f-4ff7-8ee9-ddb4fc2c4c3b.lovableproject.com",
+    "https://dental.inbrain.kz",
     "http://localhost:5173",
     "http://localhost:8080",
-    "https://dental.inbrain.kz",
-    "https://b1d6fa20-0f2f-4ff7-8ee9-ddb4fc2c4c3b.lovableproject.com"
 ]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=allow_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
